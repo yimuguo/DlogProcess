da073f8ef5be9ba2dd042bd35725f220c8161249
yimuguo <guoyimu@gmail.com>
2015-10-06 15:58:37 -0700

    Skew lot bands

---
+++ vcoband.py
@@ -0,0 +1,17 @@
+__author__ = 'yguo'
+
+import re
+
+LineBuffer = []
+with open(".\\examples\\21MY_HOT_5P49V5901_FT_754_J750-05_1A", 'r') as data:
+    SkewLotsDlog = data.read().splitlines()
+PassDlog = open('PassUnits.txt', 'w+')
+
+for line in SkewLotsDlog:
+    if "Device#:" in line:
+        NewDevice = 1
+        continue
+    LineBuffer.append(line)
+    if line[15:25] == '1         1':
+        NewDevice = 0
+        PassDlog.write
