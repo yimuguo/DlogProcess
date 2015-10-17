a503aad6406f44cb07a164ea4e32974cc50ee2ac
yimuguo <guoyimu@gmail.com>
2015-10-16 18:32:46 -0700

    process skew lot dlogs.

--- vcoband.py
+++ vcoband.py
@@ -2,17 +2,31 @@ __author__ = 'yguo'
 
 import re
 
-LineBuffer = []
-with open(".\\examples\\21MY_HOT_5P49V5901_FT_754_J750-05_1A", 'r') as data:
+
+def screen_pass(lotnumber, original_dlog):
+    data_buffer = []
+    pass_dlog = open('PassUnits_%s.txt' % lotnumber, 'w+')
+    new_device = 0
+    for line in range(0, len(original_dlog)):
+        if "Device#:" in original_dlog[line]:
+            new_device = 1
+        if new_device == 1:
+            data_buffer.append(original_dlog[line])
+        if original_dlog[line][:73] == '=========================================================================':
+            new_device = 0
+            if original_dlog[line-1][14:25] == '1         1':
+                pass_dlog.writelines(["%s\n" % item for item in data_buffer])
+            data_buffer = []
+    pass_dlog.close()
+
+with open(".\\examples\\20MY_HOT_5P49V5901_FT_754_J750-05_1A.txt", 'r') as data:
     SkewLotsDlog = data.read().splitlines()
-PassDlog = open('PassUnits.txt', 'w+')
+screen_pass('FF', SkewLotsDlog)
+with open(".\\examples\\21MY_HOT_5P49V5901_FT_754_J750-05_1A.txt", 'r') as data:
+    SkewLotsDlog = data.read().splitlines()
+screen_pass('SS', SkewLotsDlog)
 
-for line in SkewLotsDlog:
-    if "Device#:" in line:
-        NewDevice = 1
-        continue
-    LineBuffer.append(line)
-    if line[15:25] == '1         1':
-        NewDevice = 0
-        PassDlog.writelines(LineBuffer)
-
+# devices = open('PassUnits_FF.txt', 'r').read()
+# print("There are " + str(devices.count('Device#:')) + " units passed dlog in this file")
+# devices = open('PassUnits_SS.txt', 'r').read()
+# print("There are " + str(devices.count('Device#:')) + " units passed dlog in this file")
