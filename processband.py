71d5bce2d4c6923f607c486c17930303e25e2d59
yimuguo <guoyimu@gmail.com>
2015-11-11 17:21:51 -0800

    process skew lot dlogs.

--- processband.py
+++ processband.py
@@ -1,17 +1,20 @@
-from dlogprocess import *
+# from dlogprocess import *
 
 __author__ = 'Eame'
 
 
-VCOBandDatang = Dlog()
+def read_dlog(dlog_path):
+    with open(dlog_path, 'r') as data:
+        dlog_data = data.read().splitlines()
+    return dlog_data
+# VCOBandDatang = Dlog()
 BandInfo = []
 BandBuffer = []
-for unit in range(1, 99):
-    VCOBandDatang.read_dlog(
-        '.\\examples\\VCOCal\\U' + str(unit) + '.txt')
-    for x in range(0, len(VCOBandDatang.dlog_data)):
-        if 'Turn Off Test_VCO_band and Calibrate VCO Band' in VCOBandDatang.dlog_data[x]:
-            BandBuffer.append(VCOBandDatang.dlog_data[x+4][14:16])
+for unit in range(2, 21):
+    dlog_data = read_dlog('.\\data\\Roland_Data\\50C\\U' + str(unit) + '.txt')
+    for x in range(0, len(dlog_data)):
+        if 'Turn Off Test_VCO_band and Calibrate VCO Band' in dlog_data[x]:
+            BandBuffer.append(dlog_data[x+4][14:16])
     BandInfo.append(BandBuffer)
     for i in range(0, 10):
         if BandBuffer[i] != BandBuffer[i+1]:
@@ -22,9 +25,10 @@ for unit in range(1, 99):
             print("Unit " + str(unit) + " calibrated to Band Ch")
         elif i == 9 and BandBuffer[i] == "BH":
             print("Unit " + str(unit) + " calibrated to Band Bh")
+    print(BandBuffer)
     BandBuffer = []
 Band98 = []
 for line in range(0, len(BandInfo)):
-    Band98.append(BandInfo[line][0])
-print("Band Ch has %s units" % str(Band98.count('CH')))
-print("Band Bh has %s units" % str(Band98.count('BH')))
+    Band98.append(BandInfo[line][10])
+print("Band Eh has %s units" % str(Band98.count('EH')))
+print("Band Dh has %s units" % str(Band98.count('DH')))
