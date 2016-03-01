c6c7303189f015f4cf93611d4cd7edf1c9601513
yimuguo <guoyimu@gmail.com>
2016-02-29 17:21:22 -0800

    Merge branch 'master' of C:\Users\yguo\Documents\GitHub\dlogprocess with conflicts.

--- dlog2tp.py
+++ dlog2tp.py
@@ -35,9 +35,9 @@ def keyword_limit(keyword,dlog,tp_txt):
 ###Read into Datalog
 #filename=raw_input("Enter dlog full path:\n")
 
-os.chdir("S://Test_Eng//J750_HW_SW//VC5//AK652_008_772_IBM//dlog//")
+os.chdir("S:\Test_Eng\J750_HW_SW\VC5_bonding_options\VC5_bonding_custom_code\\5P49V5929Bxxx\-514\dlog\\")
 
-for file in glob.glob("*qa*.txt"):
+for file in glob.glob("*ft*.txt"):
     filename = file
 with open(filename) as data:
     dlog=data.read().splitlines()
@@ -45,8 +45,10 @@ with open(filename) as data:
 keyword_limit('IDD33_dynamic',dlog,tp_txt)
 keyword_limit('IDD33_3s',dlog,tp_txt)
 keyword_limit('IDD33_SD',dlog,tp_txt)
-keyword_limit('VOLH_HCSL33',dlog,tp_txt)
+# keyword_limit('VOLH_HCSL33',dlog,tp_txt)
 keyword_limit('VOLH33',dlog,tp_txt)
+keyword_limit('OutputLeakage', dlog, tp_txt)
+keyword_limit('InputLeakage', dlog, tp_txt)
 
 #keyword_limit('IDD',dlog,tp_txt)
 #tmin1,tmax1=keyword_limit('LDO
--- processband.py
+++ processband.py
@@ -10,25 +10,29 @@ def read_dlog(dlog_path):
 # VCOBandDatang = Dlog()
 BandInfo = []
 BandBuffer = []
-for unit in range(1, 31):
-    dlog_data = read_dlog('.\\data\\Roland_Data\\30C\\U' + str(unit) + '.txt')
+for unit in range(1, 21):
+    dlog_data = read_dlog('S:\\Test_Eng\\yGuo\\VC5_exp\\dlogs\\-20C\\U' + str(unit) + '.txt')
     for x in range(0, len(dlog_data)):
         if 'Turn Off Test_VCO_band and Calibrate VCO Band' in dlog_data[x]:
             BandBuffer.append(dlog_data[x+4][14:16])
+        # elif ' Site    Sort     Bin' in dlog_data[x]:
+        #     if dlog_data[x+2][14:15] == '1':
+        #         print("Unit" + str(unit) + " Passed Both Margin")
     BandInfo.append(BandBuffer)
-    for i in range(0, 10):
+    for i in range(0, 9):
         if BandBuffer[i] != BandBuffer[i+1]:
             print("Unit " + str(unit) + " multiple band values during calibration")
             # print(BandBuffer)
             break
-        # if i == 9 and BandBuffer[i] == 'EH':
-        #     print("Unit " + str(unit) + " calibrated to Band Eh")
-        # elif i == 9 and BandBuffer[i] == "DH":
-        #     print("Unit " + str(unit) + " calibrated to Band Dh")
-    print(str(unit), BandBuffer)
+        if i == 10 and BandBuffer[i] == 'EH':
+            print("Unit " + str(unit) + " calibrated to Band Eh")
+        elif i == 10 and BandBuffer[i] == "DH":
+            print("Unit " + str(unit) + " calibrated to Band Dh")
+    print(BandBuffer)
     BandBuffer = []
 Band98 = []
 for line in range(0, len(BandInfo)):
-    Band98.append(BandInfo[line][0])
+    Band98.append(BandInfo[line][9])
 print("Band Eh has %s units" % str(Band98.count('EH')))
 print("Band Dh has %s units" % str(Band98.count('DH')))
+print(Band98)
