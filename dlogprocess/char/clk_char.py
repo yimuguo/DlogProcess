f1bcc703cae490ebfdc089c9d4bf8ac3708d1b91
yimuguo <guyimu@gmail.com>
2016-03-15 00:27:28 -0700

    Regex on char table matching

--- dlogprocess/char/clk_char.py
+++ dlogprocess/char/clk_char.py
@@ -8,4 +8,14 @@ class CharDlog(Dlog):
         self.dlog_data = self.screen_pass(write_to_file=0)
         self.temp = temp
 
-    def
+    @staticmethod
+    def ln_match_char(str_in):
+        if re.match('^\s+[a-zA-Z]+_[a-zA-z0-9]+\s+[-+]?\d*\.\d+\s+[-+]?\d*\.\d+\s+[-+]?\d*\.\d+\s+', str_in):
+            return True
+        else:
+            return False
+
+    def find_char_table(self):
+        for line in range(0, len(self.dlog_data)):
+            if re.match('\s+=+', self.dlog_data[line]):
+                table_str = 1
