34741b00c37d58f1f6e7ec3d49d706d9e038720f
yimuguo <guyimu@gmail.com>
2016-03-16 01:15:34 -0700

    Regex on char table header, grab all the summary tables out

---
+++ dlogprocess/char/test_charDlog.py
@@ -0,0 +1,12 @@
+from unittest import TestCase
+from dlogprocess.char.clk_char import CharDlog
+
+
+class TestCharDlog(TestCase):
+    char = CharDlog(r'../data/Char/03122016.txt', '25C')
+
+    def test_find_char_table(self):
+        self.char.find_char_table()
+        print(self.char.char_table)
+        pass
+
