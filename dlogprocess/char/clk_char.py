f34ba36c804c5a35695faa9af8002cae0509eafc
yimuguo <guoyimu@gmail.com>
2016-03-16 19:03:23 -0700

    REGEX update

--- dlogprocess/char/clk_char.py
+++ dlogprocess/char/clk_char.py
@@ -11,7 +11,7 @@ class CharDlog(Dlog):
 
     @staticmethod
     def ln_match_header(str_in):
-        if re.match('^\s+[a-zA-Z]+\([a-zA-z]\)+\s+[-+]?\d*\.\d+\s+[-+]?\d*\.\d+\s+[-+]?\d*\.\d+\s+', str_in):
+        if re.match('^\s+[a-zA-Z]+\([a-zA-z]+\)+\s+[-+]?\d*\.\d+\s+[-+]?\d*\.\d+\s+[-+]?\d*\.\d+\s+', str_in):
             return True
         else:
             return False
@@ -36,6 +36,8 @@ class CharDlog(Dlog):
                         self.char_table.append(self.dlog_data[line-1])
             if table_start == 1:
                 self.char_table.append(self.dlog_data[line])
-                if re.match('^\s+$', self.dlog_data[line+1]):
+                if not (self.ln_match_char(self.dlog_data[line+1]) or
+                        self.ln_match_header(self.dlog_data[line+1]) or
+                        re.match('^\s+=+', self.dlog_data[line+1])):
                     table_start = 0
 
